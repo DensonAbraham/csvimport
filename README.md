5a9dc9dc58fa497a7eb26f9950a951388a9359c7
DensonAbraham <densonabraham98@gmail.com>
2020-07-05 15:00:44 +0530

    Fresh file upload , front end connected to backend

--- README.md
+++
@@ -1,1 +0,0 @@
-This is a Typescript code to push csv file to mysql database. It currently has no front end
--- src/routes/csv.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { uploadcsv,upload} from "../controllers/csv.controller";
-
-const router = Router();
-
-router.route("/csv")
-    .post(upload.single("document"),uploadcsv)
-
-export default router;
